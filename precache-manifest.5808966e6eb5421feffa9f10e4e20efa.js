a684d7d9edb8addb0375c8b04c5e6934d9ff130e
gaurav1620 <gauravak007@gmail.com>
2020-10-07 11:11:54 +0530

    Updates

---
+++ precache-manifest.5808966e6eb5421feffa9f10e4e20efa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a3f894fde85c14a74fe7c049aa6805",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37ecd5353be408ee5c96",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "e95f89a4357148ecb30e",
+    "url": "/static/js/2.5ef4a951.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.5ef4a951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ecd5353be408ee5c96",
+    "url": "/static/js/main.b47657bb.chunk.js"
+  },
+  {
+    "revision": "7facafd2d55a06cdc078",
+    "url": "/static/js/runtime-main.ed6784cd.js"
+  }
+]);
